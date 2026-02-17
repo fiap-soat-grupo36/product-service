295f4f9239d0838918d794376bf3f4c7bfbfd30c
Isaahs <isaahs41@gmail.com>
2026-02-17 20:01:44 -0300

    feat: Add JWT authentication filter and token provider integration

--- src/main/java/br/com/fiap/oficina/product_service/config/JwtTokenProvider.java
+++ src/main/java/br/com/fiap/oficina/product_service/config/JwtTokenProvider.java
@@ -1,12 +1,12 @@
 package br.com.fiap.oficina.product_service.config;
 
+import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.Jwts;
 import io.jsonwebtoken.security.Keys;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.security.core.GrantedAuthority;
 import org.springframework.stereotype.Component;
 
-import io.jsonwebtoken.Claims;
-import io.jsonwebtoken.Jwts;
 import javax.crypto.SecretKey;
 import java.nio.charset.StandardCharsets;
 import java.util.Collection;
