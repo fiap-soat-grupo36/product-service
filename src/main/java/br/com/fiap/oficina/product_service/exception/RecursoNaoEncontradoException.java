2fc0d32e6bf46349d693bb242b157a6f2eb9b193
Isaahs <isaahs41@gmail.com>
2026-02-17 20:34:21 -0300

    feat: Add custom exception for resource not found

---
+++ src/main/java/br/com/fiap/oficina/product_service/exception/RecursoNaoEncontradoException.java
@@ -0,0 +1,12 @@
+package br.com.fiap.oficina.product_service.exception;
+
+public class RecursoNaoEncontradoException extends RuntimeException {
+
+    public RecursoNaoEncontradoException(String message) {
+        super(message);
+    }
+
+    public RecursoNaoEncontradoException(String message, Throwable cause) {
+        super(message, cause);
+    }
+}
